4ef77be339fbf8b2dbe49f60b1514712d66ac28f
Leonardo Iribarne Paiva Magalhães <leo20042010@hotmail.com>
2025-03-23 10:28:20 -0300

    adcionado o projeto

---
+++ project/src/test/java/com/iribarne/project/MyfistprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iribarne.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfistprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
